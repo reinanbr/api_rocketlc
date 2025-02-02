b20c490cfc5211ebf5ddda72669b55bf0f4708fa
reinanbr <perseu912@gmail.com>
2025-02-02 12:19:40 -0300

    try fix error vercel pt.5 - servelles function

---
+++ api/index.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
+
+@app.get('/api/ssl') #response_model=List[LaunchInfo], summary="Obter informações de lançamentos", tags=["SSL"])
+async def ssl_info(request: Request):
+    data_launch = sll.launchs()
+    return JSONResponse(data_launch)
+
